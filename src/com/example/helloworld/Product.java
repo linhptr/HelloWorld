741e770c1a62ddea99cef36c377868b4b3d09931
linhtra92 <phuonglt@uia.no>
2021-09-09 15:47:02 +0200

    First Git commit

---
+++ src/com/example/helloworld/Product.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+public class Product {
+
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
